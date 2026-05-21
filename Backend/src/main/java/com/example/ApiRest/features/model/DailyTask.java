fbf50ae5b749ba0ae6cec3c9a5bcbff0fa43c91b
pauli13-ux <paulalopezalonso13@gmail.com>
2026-05-21 16:08:00 +0200

    Solucionar error bbdd-backend con ayuda

--- Backend/src/main/java/com/example/ApiRest/features/model/DailyTask.java
+++ Backend/src/main/java/com/example/ApiRest/features/model/DailyTask.java
@@ -2,7 +2,13 @@ package com.example.ApiRest.features.model;
 
 import com.example.ApiRest.user.model.User;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Entity
@@ -15,6 +21,7 @@ public class DailyTask {
     private String completedDay;
     private String habit;
     private String task;
+    @Column(name = "task_day") // Evita usar 'day' si es un nombre reservado, o especifica el nombre de la columna
     private String day;
 
     @ManyToOne
